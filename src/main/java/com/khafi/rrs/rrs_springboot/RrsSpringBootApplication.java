d717c150ac76a9b91aee753e1a6856d97a7ea973
Jalaluddin Khafi <Khafi98117@gmail.com>
2023-11-21 01:30:18 +0430

    the project started

---
+++ src/main/java/com/khafi/rrs/rrs_springboot/RrsSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.khafi.rrs.rrs_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RrsSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RrsSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/khafi/rrs/rrs_springboot/RrsSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khafi.rrs.rrs_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RrsSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
